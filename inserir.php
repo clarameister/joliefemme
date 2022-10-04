00135b6b188e31e812c2d4dee0cf6d6e8daa3db9
souzasss <daniele_m_souza@estudante.sesisenai.org.br>
2022-10-04 09:36:14 -0300

    04/10
    04/10

--- inserir.php
+++ inserir.php
@@ -1,28 +1,30 @@
 <?php
 include("conecta.php");
-$email = $_POST["email"];
 $nome = $_POST["nome"];
 $senha = $_POST["senha"];
 $cpf = $_POST["cpf"];
+$email = $_POST["email"];
 $telefone = $_POST["telefone"];
+$rua= $_POST["rua"];
+$numero= $_POST["numero"];
+$bairro= $_POST["bairro"];
 $cidade= $_POST["cidade"];
 $estado= $_POST["estado"];
-$rua= $_POST["rua"];
-$bairro= $_POST["bairro"];
 $nivel_admin= 0;
 
 
 
-$comando = $pdo->prepare("INSERT INTO clientes(email,nome,senha,cpf,telefone,cidade,estado,rua,bairro, nivel_admin) VALUES(:email,:nome,:senha,:cpf,:telefone,:cidade,:estado,:rua,:bairro,:nivel_admin)");
-$comando->bindValue(":email", $email);
+$comando = $pdo->prepare("INSERT INTO cliente(nome,senha,cpf,email,telefone,rua,numero,bairro,cidade,estado, nivel_admin) VALUES(:nome,:senha,:cpf,:email,:telefone,:rua,:numero,:bairro,:cidade,:estado,:nivel_admin)");
 $comando->bindValue(":nome", $nome);
 $comando->bindValue(":senha", $senha);
 $comando->bindValue(":cpf", $cpf);
+$comando->bindValue(":email", $email);
 $comando->bindValue(":telefone", $telefone);
+$comando->bindValue(":rua", $rua);
+$comando->bindValue(":numero", $numero);
+$comando->bindValue(":bairro", $bairro);
 $comando->bindValue(":cidade", $cidade);
 $comando->bindValue(":estado", $estado);
-$comando->bindValue(":rua", $rua);
-$comando->bindValue(":bairro", $bairro);
 $comando->bindValue(":nivel_admin", $nivel_admin);
 $comando->execute();
 

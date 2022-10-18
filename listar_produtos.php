054c242f9aeb35d5f95394fad83da0eb648eb2d5
souzasss <daniele_m_souza@estudante.sesisenai.org.br>
2022-10-18 09:41:14 -0300

    18/10
    18/10

---
+++ listar_produtos.php
@@ -0,0 +1,12 @@
+<?php
+
+include("conecta.php");
+
+$nome_prod = $_POST['nome_prod'];
+$preco = $_POST['preco'];
+$imagem=$_FILES['meu_upload']; 
+
+$comando = $pdo->prepare("SELECT nome_prod, preco, imagem FROM produto");
+
+$comando->execute();
+

ced936452019965da386b2c712895c52f335b046
clarameister <110902601+clarameister@users.noreply.github.com>
2022-10-27 07:47:33 -0300

    a
    a

--- listar_produtos.php
+++ listar_produtos.php
@@ -1,10 +1,17 @@
 <?php
 
+<<<<<<< Updated upstream
 include("conecta.php"); 
+=======
+include("conecta.php");
+
+$comando = $pdo->prepare("SELECT nome_prod, preco, imagem FROM produto");
+>>>>>>> Stashed changes
 
 $comando = $pdo->prepare("SELECT * FROM produto");
 $comando->execute();
 
+<<<<<<< Updated upstream
 if($comando->rowCount() >= 1){
     $lista_produtos = $comando->fetchAll();
 }else{
@@ -13,4 +20,18 @@ if($comando->rowCount() >= 1){
 
 unset($comando);
 unset($pdo);
+=======
+<<<<<<< HEAD
+=======
+if($comando->rowCount() >= 1)
+    {
+        $lista_produtos = $comando->fetchAll();
+    }else{
+        echo("Não há produtos cadastrados.");
+    }
+    unset($comando);
+    unset($pdo);
+
+>>>>>>> f050ee77960ef5acd485f6be52d3a0134cc73551
+>>>>>>> Stashed changes
 ?>

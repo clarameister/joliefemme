0ab1e4e3ac49c048f8f673868ff5db276b9330b2
souzasss <daniele_m_souza@estudante.sesisenai.org.br>
2022-11-15 13:35:39 -0300

    aa
    aa

--- conecta.php
+++ conecta.php
@@ -3,7 +3,7 @@
     date_default_timezone_set('America/Sao_Paulo');
     
     try{
-        $pdo = new PDO("mysql:dbname=joilefemme;host=localhost;charset=utf8","root","");
+        $pdo = new PDO("mysql:dbname=joliefemme;host=localhost;charset=utf8","root","");
     }
     catch(PDOException $erro)
     {

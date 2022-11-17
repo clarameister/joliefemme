b4644538ca5c658c99ad24b83f92de8fc1c04ddc
souzasss <daniele_m_souza@estudante.sesisenai.org.br>
2022-11-17 07:39:51 -0300

    aa
    aa

--- listar_consultoras.php
+++ listar_consultoras.php
@@ -1,1 +1,18 @@
-aaaaaaaa
+-<?php
+include("conecta.php");
+
+$comando = $pdo->prepare("SELECT * FROM consultoras ");
+
+$comando->execute();
+
+if($comando->rowCount() >= 1)
+{
+    $lista_consultoras = $comando->fetch();
+}else{
+    echo("Não deu certo!");
+}
+
+  unset($comando);
+  unset($pdo);
+
+?>
